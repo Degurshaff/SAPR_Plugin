15f467a2e3b543fb7717593db339441fb69c1fff
Degurshaff <ilyaexe666@gmail.com>
2021-12-21 17:26:37 +0700

    1

--- kompasSword/kompasSwordUi/mainForm.Designer.cs
+++ kompasSword/kompasSwordUi/mainForm.Designer.cs
@@ -29,10 +29,17 @@ namespace kompasSwordUi
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "mainForm";
+            this.Text = "Sword";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
